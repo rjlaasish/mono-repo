6ca55b026919ef19e75d37d324a2f6845534abd0
rjlaasish <rjlaasish@gmail.com>
2021-01-31 08:51:13 +0545

    Monorepo architecture sample

--- packages/internet-banking/src/App.js
+++ packages/internet-banking/src/App.js
@@ -1,27 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import {Button, InputText} from "@batman/ui-components";
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button/>
-        <InputText/>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <Button/>
+          <InputText/>
+        </header>
+
+      </div>
   );
 }
 
